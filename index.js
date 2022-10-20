9b36d4a4d17d7d26d053fe5de388759fd48affd3
Marcelo Gomes <mgomes.dev@gmail.com>
2022-10-20 09:30:03 -0300

    merged develop

--- index.js
+++ index.js
@@ -1,3 +1,52 @@
+//test isIn
+//test assoc, assocIf, dissoc
+//test get
+//test arrayToMap
+//test bind
+//test evolve
+//test filterSelect, select
+//test invokeAndBypass
+
+const arrayToMap = (key, funcOrArray, maybeArray) => {
+    const [arr, func] = maybeArray ? [maybeArray, funcOrArray] : [funcOrArray];
+
+    return arr.reduce((map, item) => {
+        return func ?
+            assoc(map, item[key], func(item)) :
+            assoc(map, item[key], item);
+    }, {});
+};
+
+const assoc = (obj, key, value, ...nextKeysValues) => {
+    if (obj && key && typeOf(obj) === "object") {
+        obj[key] = value;
+
+        return nextKeysValues ?
+            assoc(obj, ...nextKeysValues) :
+            obj;
+    } else {
+        return obj;
+    }
+};
+
+const assocIf = (obj, key, value, ...nextKeysValues) => {
+    if (obj && key && typeOf(obj) === "object") {
+        if (!isIn([null, undefined], value)) {
+            obj[key] = value;
+        };
+
+        return nextKeysValues ?
+            assoc(obj, ...nextKeysValues) :
+            obj;
+    } else {
+        return obj;
+    }
+};
+
+const bind = (func, ...args) => {
+    return func.bind(func, ...args);
+};
+
 const compose = (...funcs) => {
     return (...args) => {
         return funcs.reduceRight((arg, func, index, fullArr) => {
@@ -6,6 +55,72 @@ const compose = (...funcs) => {
             return index === fullArr.length - 1 ? func(...arg) : func(arg);
         }, args);
     };
+};
+
+const dissoc = (obj, key, ...nextKeys) => {
+    if (obj && key && typeOf(obj) === "object") {
+        delete obj.key;
+
+        return nextKeys ?
+            dissoc(obj, ...nextKeys) :
+            obj;
+    } else {
+        return obj;
+    }
+};
+
+const evolve = (transformations, obj) => {
+    const transform = (obj, [key, funcOrChildObj]) => {
+        if (typeOf(funcOrChildObj) === "function") {
+            obj[key] = funcOrChildObj(obj[key]);
+        } else {
+            transform(obj[key], Object.entries(funcOrChildObj)[0]);
+        };
+
+        return obj;
+    };
+
+    return Object.entries(transformations).reduce(transform, obj);
+};
+
+const invokeAndBypass = (func, value) => {
+    func(value);
+
+    return value;
+};
+
+const isIn = (valuesToCompare, value) => {
+    return valuesToCompare.some(valueToCompare => value === valueToCompare)
+};
+
+const filterSelect = (validationFunc, keyNames, obj) => {
+    return keyNames.reduce((acc, keyOrkeys) => {
+        const [originalKeyOrKeys, newKeyName] = typeOf(keyOrkeys) === "array" ?
+            keyOrkeys :
+            [keyOrkeys, keyOrkeys];
+
+        const value = get(originalKeyOrKeys, obj);
+
+        if (validationFunc(value)) {
+            assoc(acc, newKeyName, value);
+        };
+
+        return acc;
+    }, {});
+};
+
+const get = (keyOrKeys, obj) => {
+    if (obj && keyOrKeys && typeOf(obj) === "object") {
+        const [key, ...nextKeys] = typeOf(keyOrKeys) === "array" ?
+            keyOrKeys :
+            [key];
+
+        return nextKeys.length ?
+            get(nextKeys, obj) :
+            obj[key];
+    } else {
+        return obj;
+    }
 };
 
 const numberTypeOf = (numb) => {
@@ -48,6 +163,20 @@ const pipe = (...funcs) => {
     };
 };
 
+const select = (keyNames, obj) => {
+    return keyNames.reduce((acc, keyOrkeys) => {
+        const [originalKeyOrKeys, newKeyName] = typeOf(keyOrkeys) === "array" ?
+            keyOrkeys :
+            [keyOrkeys, keyOrkeys];
+
+        const value = get(originalKeyOrKeys, obj);
+
+        assoc(acc, newKeyName, value);
+
+        return acc;
+    }, {});
+};
+
 const typeOf = (input) => {
     const jsTypeOf = typeof input;
 
@@ -65,7 +194,18 @@ const typeOf = (input) => {
 };
 
 module.exports = {
-    typeOf,
+    arrayToMap,
+    assoc,
+    assocIf,
+    bind,
+    compose,
+    dissoc,
+    evolve,
+    filterSelect,
+    get,
+    invokeAndBypass,
+    isIn,
     pipe,
-    compose
+    select,
+    typeOf
 };
