466b5d8ab765abf393a858fcaacc4c7ba9e4d45b
MGomes-Dev <mgomes.dev@gmail.com>
2023-03-14 01:03:59 -0300

    fix: bug on recursive merge call

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ const merge = (originalObj, objToMerge) => {
         const originalValueIsObj = typeOf(originalValue) === "object";
 
         if (newValueIsObj && originalValueIsObj) {
-            assocDeep(oldValue, value)
+            merge(oldValue, value)
         } else {
             originalObj[key] = value;
         }
